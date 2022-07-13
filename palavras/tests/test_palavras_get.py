1951a9ca2f22195e32d3b4556a126ee085097a8a
odiegofarias <diego.farias.fernandes@gmail.com>
2022-07-13 20:58:55 -0300

    Template básico da página adicionado com testes

--- palavras/tests/test_palavras_get.py
+++ palavras/tests/test_palavras_get.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from pytest_django.asserts import assertContains
 
 
 
@@ -11,3 +12,11 @@ def resposta(client):
 
 def test_status_code(resposta):
     assert resposta.status_code == 200
+
+
+def test_se_o_formulario_esta_na_pagina(resposta):
+    assertContains(resposta, '<form')
+
+
+def test_se_o_botao_salvar_eh_do_tipo_submit(resposta):
+    assertContains(resposta, '<button type="submit"')
